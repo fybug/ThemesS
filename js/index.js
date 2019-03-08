5b0cc3a0da12df72c18d1d8285c65f41c0d2ba1b
fybug <1006291762@qq.com>
2019-03-09 01:30:59 +0800

    [???]

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
 $(".frmwork>.foot>.headAndFoot>a").click(function () {
     alert('还没想好放什么')
 });
-// 先隐藏展示区域
-$("#see").hide(0);
+// 点击展示图片
 $(".showS>section>.conins>div>img").click(function () {
     var s = $("#see");
-    s.children("img").attr("src", $(this).attr('src'));
-    s.show(0);
+    var img = s.children("#screen");
+
+    s.removeClass("hidenow");
+
+    img.css("background-image", "url(" + $(this).attr('src') + ")");
 });
 // 展示区域点击隐藏
 $("#see").click(function () {
-    var self = $("#see");
-    self.children("img").attr("src", "");
-    self.hide(0);
+    $("#see").addClass('hidenow');
     return false;
+});
+// 设置捐献隐藏
+$("#aoutDonate").click(function () {
+    $("#aoutDonate").addClass('hidenow');
+    return false;
+});
+// 设置捐献按钮
+$(".aouter>button").click(function () {
+    var aout = $(this).siblings("a").children("img").attr('title');
+
+    $("#aoutDonate").removeClass("hidenow");
+
+    $("#qqplay").css('background-image', "url(static/img/" + aout + "-" + "qq)");
+    $("#wechatplay").css('background-image', 'url(static/img/' + aout + '-' + 'wechat)');
+    $("#ailiplay").css('background-image', 'url(/static/img/' + aout + '-' + 'aili)');
 });
